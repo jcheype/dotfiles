d46e500b500727ad861ad2de723bfbf0282ae268
Julien Cheype <cheype@gmail.com>
2024-10-31 01:40:52 +0100

    update nvim and cleanup

---
+++ .config/nvim.bak2/lua/plugins/init.lua
@@ -0,0 +1,59 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+ {
+   "neovim/nvim-lspconfig",
+   config = function()
+       require("nvchad.configs.lspconfig").defaults()
+       require "configs.lspconfig"
+   end,
+ },
+  {
+    "nvimtools/none-ls.nvim",
+    event= "VeryLazy",
+    opts = function ()
+      return require "configs.null-ls"
+    end
+  },
+  --
+  -- {
+  -- 	"williamboman/mason.nvim",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"lua-language-server", "stylua",
+  -- 			"html-lsp", "css-lsp" , "prettier"
+  -- 		},
+  -- 	},
+  -- },
+  --
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css"
+  		},
+  	},
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    event = "VeryLazy",
+    opts = function()
+      require("neo-tree").setup()
+    end
+  }
+}
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,58 +2,24 @@ return {
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
-    config = function()
-      require "configs.conform"
-    end,
+    opts = require "configs.conform",
   },
 
   -- These are some examples, uncomment them if you want to see them work!
- {
-   "neovim/nvim-lspconfig",
-   config = function()
-       require("nvchad.configs.lspconfig").defaults()
-       require "configs.lspconfig"
-   end,
- },
   {
-    "nvimtools/none-ls.nvim",
-    event= "VeryLazy",
-    opts = function ()
-      return require "configs.null-ls"
-    end
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
   },
-  --
+
   -- {
-  -- 	"williamboman/mason.nvim",
+  -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
   -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
   -- 		},
   -- 	},
   -- },
-  --
-  {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css"
-  		},
-  	},
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    event = "VeryLazy",
-    opts = function()
-      require("neo-tree").setup()
-    end
-  }
 }
