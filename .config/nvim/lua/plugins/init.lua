c455d14f922aeb3c6236a594c5f29a9986b7693d
Julien Cheype <cheype@gmail.com>
2024-11-03 00:00:11 +0100

    install and fix rust-analyzer

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -12,7 +12,22 @@ return {
       require "configs.lspconfig"
     end,
   },
+ { 'mfussenegger/nvim-dap'},
 
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim", "lua", "vimdoc",
+        "html", "css"
+      },
+    },
+  },
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^4', -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },   
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
