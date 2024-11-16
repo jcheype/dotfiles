f822c3670a6695ef8fbaaeb01abef2c6427b3082
Julien Cheype <cheype@gmail.com>
2024-11-16 13:07:32 +0100

    update jj starship

--- home-manager/darwin.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-   home.username = "julien";
-   home.homeDirectory = "/Users/julien";
-}
