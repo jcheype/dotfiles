d72a05c4ed952c55e2c4faaa3b1adcab7ede4b99
Julien Cheype <cheype@gmail.com>
2025-04-24 14:07:04 +0200

    update atuin and jj and nixdarwin

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -57,10 +57,8 @@
     pkgs.nodejs
 
     pkgs.google-chrome
-    #pkgs.bruno
     pkgs.vscode
 
-    pkgs.zig
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
